2dd1b3c2c5481adf16564bee85029b2a55dd1339
Megunuts <116467997+Megunuts@users.noreply.github.com>
2023-11-09 10:14:26 -0600

    delete button working for layouts

--- src/app/layouts.service.ts
+++ src/app/layouts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, of } from 'rxjs';
 import { LayoutData } from './Layouts';
@@ -9,7 +9,9 @@ import { LayoutData } from './Layouts';
 export class LayoutsService {
 
   private url:string = 'http://localhost:9003/layouts'
-
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
   constructor(private http: HttpClient) {
 
    }
@@ -19,7 +21,9 @@ export class LayoutsService {
       .get<LayoutData[]>(this.url)
       .pipe(catchError(this.handleError<LayoutData[]>('getLayouts', [])))
    }
-
+   deleteLayout(id: number): Observable<any>{
+    return this.http.delete<any>(`${this.url}/${id}`, this.httpOptions).pipe(catchError(this.handleError<any>('getLayouts')));
+    }
    private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
