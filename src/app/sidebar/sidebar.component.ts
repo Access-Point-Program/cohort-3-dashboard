99d9a647109a6ed377ceb8479bada0ca63078810
nnaucke <noahnaucke@gmail.com>
2023-11-20 14:34:53 -0600

    sidebar update 1

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-  active: string = 'r';
+  active: string = 'd';
 
   change(s:string){
     this.active = s;
