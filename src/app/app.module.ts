67456a124155c8a08527ea121f75809cf300414f
nickjansen12312 <nickjansen12312@gmail.com>
2023-12-06 15:13:10 -0600

    added testing for ruleset table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,32 @@
 // Importing necessary Angular modules
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 // Importing components and routing module
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { LayoutsTableComponent} from './table/table.component';
-import { RulesetsComponent } from './rulesets/rulesets.component';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { LayoutsTableComponent } from "./table/table.component";
+import { RulesetsComponent } from "./rulesets/rulesets.component";
+import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
-        // Declaring components used in the module
+    // Declaring components used in the module
     AppComponent,
     SidebarComponent,
     LayoutsTableComponent,
     RulesetsComponent,
   ],
   imports: [
-        // Importing Angular modules for functionality
+    // Importing Angular modules for functionality
     BrowserModule,
     HttpClientModule,
-    NgbModule,// ng-bootstrap module for UI components
-    AppRoutingModule// Custom routing module
+    NgbModule, // ng-bootstrap module for UI components
+    AppRoutingModule, // Custom routing module
   ],
-  providers: [],// Service providers can be added here
-  bootstrap: [AppComponent]// The root component to be bootstrapped
+  providers: [], // Service providers can be added here
+  bootstrap: [AppComponent], // The root component to be bootstrapped
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/rulesets/rulesets.component.spec.ts
+++ src/app/rulesets/rulesets.component.spec.ts
@@ -1,33 +1,115 @@
 // Importing necessary modules for Angular unit testing
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+
 
 // Importing the component to be tested
-import { RulesetsComponent } from './rulesets.component';
+import { RulesetsComponent } from "./rulesets.component";
+import { of } from 'rxjs';
+import { RulesetsService } from "../rulesets.service";
+import { NgbPaginationModule } from "@ng-bootstrap/ng-bootstrap";
 
-// Test suite for the RulesetsComponent
-describe('RulesetsComponent', () => {
-  // Variables to hold the component instance and the testing fixture
-  let component: RulesetsComponent;
-  let fixture: ComponentFixture<RulesetsComponent>;
 
-  // Setup function to be executed before each test
-  beforeEach(() => {
-    // Configuring the testing module with the RulesetsComponent
-    TestBed.configureTestingModule({
-      declarations: [RulesetsComponent]
+  describe('RulesetComponent', () => {
+    let component: RulesetsComponent;
+    let fixture: ComponentFixture<RulesetsComponent>;
+    let rulesetService: Partial<RulesetsService>;
+
+
+    beforeEach(async () => {
+      rulesetService = {
+        getRulesets: () => of([]),
+        deleteRuleset: (id: number) => of({}),
+      };
+
+
+      await TestBed.configureTestingModule({
+        declarations: [RulesetsComponent],
+        providers: [
+          { provide: RulesetsService, useValue: rulesetService},
+        ],
+        imports: [HttpClientTestingModule, HttpClientModule, NgbPaginationModule],
+      }).compileComponents();
     });
 
-    // Creating a component fixture and getting the component instance
-    fixture = TestBed.createComponent(RulesetsComponent);
-    component = fixture.componentInstance;
+    beforeEach(() => {
+      fixture = TestBed.createComponent(RulesetsComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
 
-    // Detecting changes in the component (e.g., triggering ngOnInit)
-    fixture.detectChanges();
-  });
+    fit('Creates component', () => {
+      expect(component).toBeTruthy();
+    });
 
-  // Test case: checking if the component is created successfully
-  it('should create', () => {
-    // Expecting that the component instance is truthy, indicating successful creation
-    expect(component).toBeTruthy();
+
+
+    fit('should call getRuleset when started', () => {
+      spyOn(component, 'getRulesets');
+      component.ngOnInit();
+      expect(component.getRulesets).toHaveBeenCalled();
+    });
+
+
+
+    fit('refresh rulesets when page is changed', () => {
+      const rulesets = [
+        { id: 1, name: 'Ruleset 1', creator: 'Nick', creationDate: new Date()},
+        { id: 2, name: 'Ruleset 2', creator: 'Noah', creationDate: new Date()}
+      ];
+
+      component.rulesets = rulesets;
+      component.page = 2;
+      component.pageSize = 1;
+
+      component.refreshRulesets();
+
+      expect(component.rulesetsData).toEqual([rulesets[1]]);
+    });
+
+
+
+    fit('shows confirmation box when trashcan is pressed', () => {
+      const itemId = 1;
+
+      component.confirmDelete(itemId);
+      expect(component.showConfirmation).toBe(true);
+    });
+
+
+
+    fit('delete a ruleset calls with id', () => {
+      const itemId = 1;
+      spyOn(component['rulesetService'], 'deleteRuleset').and.callThrough();
+      
+      component.itemIdToDelete = itemId;
+      component.delete();
+
+      expect(component['rulesetService'].deleteRuleset).toHaveBeenCalledWith(itemId);
+    });
+
+
+
+    fit('cancel delete gets rid of confirmation box', () => {
+      component.cancelDelete();
+
+      expect(component.showConfirmation).toBe(false);
+    });
+
+    fit('should retrieve data from the service', () => {
+      const rulesets = [
+        { id: 1, name: 'Ruleset 1', creator: 'Nick', creationDate: new Date()},
+        { id: 2, name: 'Ruleset 2', creator: 'Noah', creationDate: new Date()}
+      ];
+
+      spyOn(component['rulesetService'], 'getRulesets').and.returnValue(of(rulesets));
+      component.getRulesets();
+
+      expect(component.rulesets).toEqual(rulesets);
+    });
+
+
+
+
   });
-});
