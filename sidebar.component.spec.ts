e72d34c32387a1a96c1f4b8709553af7a090d787
TyeshaWh <121519281+TyeshaWh@users.noreply.github.com>
2023-12-08 13:09:09 -0600

    Add files via upload
    added unit testing for sidebar

---
+++ sidebar.component.spec.ts
@@ -0,0 +1,48 @@
+// Importing necessary modules for Angular unit testing
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// Importing the component to be tested
+import { SidebarComponent } from './sidebar.component';
+
+// Test suite for the SidebarComponent
+describe('SidebarComponent', () => {
+  // Variables to hold the component instance and the testing fixture
+  let component: SidebarComponent;
+  let fixture: ComponentFixture<SidebarComponent>;
+
+  // Setup function to be executed before each test
+  beforeEach(() => {
+    // Configuring the testing module with the SidebarComponent
+    TestBed.configureTestingModule({
+      declarations: [SidebarComponent]
+    });
+
+    // Creating a component fixture and getting the component instance
+    fixture = TestBed.createComponent(SidebarComponent);
+    component = fixture.componentInstance;
+
+    // Detecting changes in the component (e.g., triggering ngOnInit)
+    fixture.detectChanges();
+  });
+
+  // Test case: checking if the component is created successfully
+  fit('should create', () => {
+    // Expecting that the component instance is truthy, indicating successful creation
+    expect(component).toBeTruthy();
+  });
+});
+
+describe('Sidebar Component', () => {
+  fit('should change the active state', () => {
+    const sidebar = new SidebarComponent();
+    sidebar.active = 'd';
+    sidebar.change('r');
+    expect(sidebar.active).toEqual('r');
+  });
+
+  
+  fit('should initialize with default active state', () => {
+    const sidebar = new SidebarComponent();
+    expect(sidebar.active).toEqual('d');
+  });
+});
